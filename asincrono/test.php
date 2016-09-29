58d675c0813fa812476a8a7fcc8ef56a3011eaa6
proyeccion <guevaraodiug1996@gmail.com>
2016-09-28 19:08:51 -0500

    holaweb

---
+++ asincrono/test.php
@@ -0,0 +1,25 @@
+<?php
+	$persona['paterno']="Flores";
+	$persona['materno']="Apaza";
+	$persona['nombres']="Magaly";
+	$persona['personas_id']=8;
+	$personas[]= $personas;
+	$personas[]= $personas;
+
+	$respuesta['resultado']= 2;
+	$respuesta['personas']= $personas;
+	
+	
+	echo json_encode($respuesta);
+	
+
+	$personas=array();
+	
+	while ($fila = mysqli_fetch_array($result)){
+// Operaciones con los resultados que tenemos en $fila
+}
+		
+		
+
+	 ?>
+	
