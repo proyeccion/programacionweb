cb539f8baaf146c7fec90a183fd793f5aefa67e9
proyeccion <guevaraodiug1996@gmail.com>
2016-10-18 17:06:14 -0500

    actualisacion total

--- crud/assets/js/asincrono.js
+++ crud/assets/js/asincrono.js
@@ -16,49 +16,72 @@ $(
 					$("<div>").attr({"class":"collapse navbar-collapse", "id":"navbar"}).append(
 						$("<ul>").attr({"class":"nav navbar-nav"}).append(
 							$("<li>").attr({"class":"inicio"}).append( $("<a>").text("Inicio").attr({"id":"a-inicio"}) ),
-							
 							$("<li>").append( $("<a>").text( "Nuevo"     ).attr({"id":"a-nuevo"   }) ),
 							$("<li>").append( $("<a>").text( "Contacto"  ).attr({"id":"a-contacto"}) ),
 							$("<li>").append( $("<a>").text( "Servicio"  ).attr({"id":"a-servicio"}) ),
-							$("<li>").append( $("<a>").text( "Productos" ).attr({"id":"a-productos"}) )
+							$("<li>").append( $("<a>").text( "Productos" ).attr({"id":"a-producto"}) )
 						)
 					)
 				)
 			),
 			$("<div>").attr({"class":"container"}).append(
 				$("<div>").attr({"class":"row"}).append(
-					$("<div>").attr({"class":"col-md-8"}).append(
+					$("<div>").attr({"class":"col-md-8", "id":"div-principal"}).append(
 						$("<h1>").text("Personas")
 					),
-					$("<div>").attr({"class":"col-md-4"})
+					$("<div>").attr({"class":"col-md-4"}).append(
+						$("<button>").attr({'type':'button','class':'btn btn-primary btn-lg btn-block', 'id':'crud-nuevo'}).text('Registrar Nueva Persona')
+					)
 
 				)
 			)
 		);
 
 		$("#a-inicio").on("click", function(){
-			//alert("Inicio");
-			herramienta.saluda();
-			herramienta.getNombre();
+			alert("Inicio");
+		});
 
+		$("#crud-nuevo").on("click", function(e){
+			$("#div-principal").empty().append(
+				$("<h4>").attr({"class":"text-primary"}).text("Registrar Nueva Persona"),
+				$("<form>").attr({"class":"form-horizontal"}).append(
+					$("<div>").attr({"class":"form-group"}).append(
+						$("<label>").attr({"for":"input-paterno", "class":"col-sm-2 control-label"}).text("Paterno"),
+						$("<div>").attr({"class":"col-sm-10"}).append(
+							$("<input>").attr({"type":"text", "class":"form-control", "id":"input-paterno", "placeholder":"Apellido Paterno"})
+						)
+					),
+					$("<div>").attr({"class":"form-group"}).append(
+						$("<label>").attr({"for":"input-materno", "class":"col-sm-2 control-label"}).text("materno"),
+						$("<div>").attr({"class":"col-sm-10"}).append(
+							$("<input>").attr({"type":"text", "class":"form-control", "id":"input-materno", "placeholder":"Apellido materno"})
+						)
+					),
+					$("<div>").attr({"class":"form-group"}).append(
+						$("<label>").attr({"for":"input-nombres", "class":"col-sm-2 control-label"}).text("nombres"),
+						$("<div>").attr({"class":"col-sm-10"}).append(
+							$("<input>").attr({"type":"text", "class":"form-control", "id":"input-nombres", "placeholder":"nombres"})
+						)
+					)
+					
+
+
+
+
+				)
+			);
+			e.preventDefault();
 		});
-		$("#a-nuevo").on("click", function(){
-			alert("nuevo");
-			
-		});
-		$("#a-contacto").on("click", function(){
-			alert("contacto");
-			
-		});
-		
-		$("#a-servicio").on("click", function(){
-			alert("servicio");
-			
-		});
-		$("#a-productos").on("click", function(){
-			alert("producto");
-			
-		});
+
+
+
+
+							
+						
+				
+				
+	
+
 
 
 	}
